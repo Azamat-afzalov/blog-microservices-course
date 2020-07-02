c5a712d4a86747bf9e26abad01773354e86a1f01
Azamat-afzalov <52107423+Azamat-afzalov@users.noreply.github.com>
2020-07-02 16:11:28 +0500

    completed react app and comment,post microservices. Event-bus initialized

--- blog/client/src/PostCreate.jsx
+++ blog/client/src/PostCreate.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-
+import React,{ useState } from 'react';
+import axios from 'axios';
 const PostCreate = () => {
+    const [title , setTitle] = useState('');
+    const onSubmit = async(e) => {
+        e.preventDefault();
+        const request = await axios.post('http://localhost:4000/posts',{
+            title
+        });
+        setTitle('');
+    }
     return (
         <div>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-group">
                     <label htmlFor="input">Title</label>
-                    <input type="text" className='form-control' id='input'/>
+                    <input
+                        value={title}
+                        onChange={ e => setTitle(e.target.value)}
+                        type="text"
+                        className='form-control'
+                        id='input'
+                    />
                 </div>
                 <button type='submit' className='btn btn-primary'>
                     Submit
